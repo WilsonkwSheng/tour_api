08551558801bfcb522a5e812da8890afded278a4
Wilson <imweesheng@gmail.com>
2024-03-29 10:37:39 +0800

    Setup routes for tour host and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tour_hosts, only: [:create, :show]
+  post '/tour_hosts_auth/login', to: 'tour_hosts_authentication#login'
 end
