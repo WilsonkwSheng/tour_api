f6aae9414476a6cad4c4e72f32592dbdc5095cc3
Wilson <imweesheng@gmail.com>
2024-03-29 16:02:56 +0800

    Fix wrong syntax in tour host migration

--- db/migrate/20240329015414_create_tour_hosts.rb
+++ db/migrate/20240329015414_create_tour_hosts.rb
@@ -2,7 +2,7 @@ class CreateTourHosts < ActiveRecord::Migration[7.0]
   def change
     create_table :tour_hosts do |t|
       t.string :name, null: false
-      t.string :email, null: false, unique: true
+      t.string :email, null: false
       t.string :password_digest, null: false
       t.text :description
 
