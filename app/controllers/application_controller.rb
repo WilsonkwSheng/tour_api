9df02c022aaf5bb2f2e43863457b103e0cdcaf44
Wilson <imweesheng@gmail.com>
2024-03-29 11:15:26 +0800

    Add authorize_tour_host_request for authenticate jwt token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  def authorize_tour_host_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header).with_indifferent_access
+      @tour_host = TourHost.find(@decoded[:tour_host_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  describe '#authorize_tour_host_request' do
+    let(:tour_host) { FactoryBot.create(:tour_host) }
+    let(:valid_token) { JsonWebToken.encode(tour_host_id: tour_host.id) }
+
+    context 'when authorization header is present with a valid JWT token' do
+      before do
+        request.headers['Authorization'] = "Bearer #{valid_token}"
+      end
+
+      it 'sets @tour_host with the decoded tour host' do
+        expect(controller).to receive(:render).never
+        controller.send(:authorize_tour_host_request)
+        expect(assigns(:tour_host)).to eq(tour_host)
+      end
+    end
+
+    context 'when authorization header is present with an invalid JWT token' do
+      before do
+        request.headers['Authorization'] = 'Bearer invalid_token'
+      end
+
+      it do
+        expect(controller).to receive(:render).with(json: { errors: 'Not enough or too many segments' }, status: :unauthorized)
+        controller.send(:authorize_tour_host_request)
+      end
+    end
+
+    context 'when authorization header is missing' do
+      before do
+        request.headers['Authorization'] = nil
+      end
+
+      it do
+        expect(controller).to receive(:render).with(json: { errors: 'Nil JSON web token' }, status: :unauthorized)
+        controller.send(:authorize_tour_host_request)
+      end
+    end
+  end
+end
