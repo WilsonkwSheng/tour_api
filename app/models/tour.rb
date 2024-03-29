c841bed94c50a8a1df3a0c8fbe388342be6e067b
Wilson <imweesheng@gmail.com>
2024-03-29 15:21:42 +0800

    Add association for tour has many itineraries

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,5 +1,6 @@
 class Tour < ApplicationRecord
   belongs_to :tour_host
+  has_many :itineraries, dependent: :destroy
 
   validates :title, :description, :region, :city, :travel_type, presence: true
 end
--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Tour, type: :model do
+  it { should have_many(:itineraries) }
   it { should belong_to(:tour_host) }
   it { should validate_presence_of :title }
   it { should validate_presence_of :description }
